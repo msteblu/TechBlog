a87509c532e20ee5f3f7cb14f5e0d84992c2b721
Megan Steblay <megan@steblay.net>
2021-07-22 01:22:23 -0500

    Updating the routes for the apis

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,14 @@ const router = require("express").Router();
 
 const loginRoutes = require("./login-routes");
 const signupRoutes = require("./signup-routes");
+const blogRoutes = require("./blog-routes");
+const commentRoutes = require("./comment-routes");
+const editpostRoutes = require("./editpost.routes");
 
 router.use("/login", loginRoutes);
 router.use("/signup", signupRoutes);
+router.use("/blog", blogRoutes);
+router.use("/comment", commentRoutes);
+router.use("/editpost", editpostRoutes);
 
 module.exports = router;
