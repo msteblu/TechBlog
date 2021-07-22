847db7c96022843c3feb56f25693ed8e1ab817a2
Megan Steblay <megan@steblay.net>
2021-07-22 01:10:03 -0500

    Updating event listeners for comments

---
+++ public/js/comment.js
@@ -0,0 +1,23 @@
+const commentFormHandler = async (event) => {
+  event.preventDefault();
+
+  const comment = document.querySelector("#comment-login").value.trim();
+
+  if (comment) {
+    const response = await fetch("/api/comment", {
+      method: "POST",
+      body: JSON.stringify({ comment }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/dashboard");
+    } else {
+      alert("Failed to create comment.");
+    }
+  }
+};
+
+document
+  .querySelector(".comment-form")
+  .addEventListener("submit", commentFormHandler);
